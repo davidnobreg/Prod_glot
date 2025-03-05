5f6b1d1efca5d1c0bb899bceea9e86e374b0f1de
davidnobreg <davidsantos@ads.fiponline.edu.br>
2025-03-05 13:43:35 -0300

    update

--- core/settings.py
+++ core/settings.py
@@ -134,7 +134,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 
 MEDIA_URL = '/media/'
 

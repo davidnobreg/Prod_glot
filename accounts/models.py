8d61c951e0376deea11c63ffda9b6f01ddad5b38
davidnobreg <davidsantos@ads.fiponline.edu.br>
2025-04-05 09:55:02 -0300

    update

--- accounts/models.py
+++ accounts/models.py
@@ -12,3 +12,8 @@ class User(AbstractUser):
     tipo_usuario = models.CharField(max_length=1, choices=choices_tipo_usuario)
     creci = models.CharField('Creci', max_length=50, blank=True)
     contato = models.CharField('Contato', max_length=15, blank=True)
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+        ordering = ['last_name']

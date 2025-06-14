f4c3ca3c6fd1870b75931ebdb65e70df98f0e95e
davidnobreg <davidsantos@ads.fiponline.edu.br>
2025-06-14 10:46:21 -0300

    update

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,18 +1,21 @@
 from django import forms
-from django.contrib.auth import forms
+from django.contrib.auth.forms import UserCreationForm as BaseUserCreationForm, UserChangeForm as BaseUserChangeForm
 from .models import User
 
 
-class UserChangeForm(forms.UserChangeForm):
-    class Meta(forms.UserChangeForm.Meta):
+class UserCreationForm(BaseUserCreationForm):
+    class Meta(BaseUserCreationForm.Meta):
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'creci', 'contato', 'tipo_usuario']
+
+
+class UserChangeForm(BaseUserChangeForm):
+    class Meta(BaseUserChangeForm.Meta):
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'creci', 'contato', 'tipo_usuario']
 
 
-
-
-class UserCreationForm(forms.UserCreationForm):
-    class Meta(forms.UserCreationForm.Meta):
-        model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'creci', 'contato', 'tipo_usuario']
-
+def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    for field in self.fields.values():
+        field.widget.attrs['class'] = 'form-control'

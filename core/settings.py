db31270762f64f0cc868decdb37ef8c150cee667
davidnobreg <davidsantos@ads.fiponline.edu.br>
2025-11-12 20:22:00 -0300

    update

--- core/settings.py
+++ core/settings.py
@@ -165,11 +165,11 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 

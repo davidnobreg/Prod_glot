8720de633a556b96ac625f6dd49b2a5320ab9e00
davidnobreg <davidsantos@ads.fiponline.edu.br>
2025-11-17 09:30:25 -0300

    update

--- core/settings.py
+++ core/settings.py
@@ -10,17 +10,17 @@ from decouple import Config, RepositoryEnv
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Caminho da pasta conf/.env
-#ENV_PATH = BASE_DIR / 'configuration' / '.env'
+ENV_PATH = BASE_DIR / 'configuration' / '.env'
 
 # Carrega o arquivo .env desta pasta
-#config= Config(RepositoryEnv(ENV_PATH))
+config= Config(RepositoryEnv(ENV_PATH))
 config_host = Configuration()
-config = Configuration()
+#config = Configuration()
 
 # Suas variáveis
 SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = config_host('DEBUG', default=False, cast=config_host.boolean)
+DEBUG = config('DEBUG', default=False, cast=config_host.boolean)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=config_host.list)
 

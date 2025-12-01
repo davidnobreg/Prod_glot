7b0786874a5abfdeae55ccdb85790865357aa104
David Nóbrega <davidsantos@ads.fiponline.edu.br>
2025-12-01 12:30:58 -0300

    update de coreção de mensagens

--- mensagem/services.py
+++ mensagem/services.py
@@ -17,14 +17,14 @@ class EvolutionService:
         """
         Envia mensagem via Evolution API.
         """
-        print(numero, mensagem)
+
         url = f"{self.server_url}/message/sendText/{self.instance}"
         payload = {
             "number": numero,
             "text": mensagem
             #"textMessage": {"text": mensagem}
         }
-        print(payload)
+
 
         if options:
             payload["options"] = options

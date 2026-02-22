96fd62782efb80919e943204da76cb860807e497
David Nóbrega <davidsantos@ads.fiponline.edu.br>
2026-02-22 13:52:08 -0300

    update

--- vendas/views.py
+++ vendas/views.py
@@ -1,4 +1,6 @@
 import locale
+from time import process_time_ns
+
 from dateutil.tz import tzname_in_python2
 from django.db import transaction
 from django.shortcuts import get_object_or_404, render, redirect
@@ -43,24 +45,35 @@ def selectVenda(request, venda_id):
 def reservado(request, uuid):
     lote = get_object_or_404(Lote, uuid=uuid)
     venda = RegisterVenda.objects.filter(lote=lote).first()
-    clienteContato = ClienteTelefone.objects.filter(cliente=venda.cliente).first()
 
+    clienteContato = None
 
-    # Verifica se o lote está marcado como vendido mas não possui venda registrada
+    # Se existir venda, busca o telefone
+    if venda:
+        clienteContato = ClienteTelefone.objects.filter(
+            cliente=venda.cliente
+        ).first()
+
+    # Se estiver vendido mas não existir venda registrada
     if lote.situacao.lower() == 'vendido' and venda is None:
-        return render(request, 'reservado.html', {'lote': lote})
+        return render(request, 'reservado.html', {
+            'lote': lote,
+            'reservas': None,
+            'contatoCliente': None
+        })
 
-    # Caso contrário, segue para o template padrão
     context = {
         'contatoCliente': clienteContato,
         'reservas': venda,
-        'lote': lote}
+        'lote': lote
+    }
+
     return render(request, 'reservado.html', context)
 
 
 @has_permission_decorator('reservadoDetalhe')
-def reservadoDetalhe(request, id):
-    reservas = RegisterVenda.objects.filter(lote_id=id).first()
+def reservadoDetalhe(request, reserva_uuid):
+    reservas = RegisterVenda.objects.filter(lote__uuid=reserva_uuid).first()
     context = {'reservas': reservas}
     return render(request, 'reservado_detalhe.html', context)
 
@@ -119,7 +132,7 @@ def listaReserva(request):
 
 @has_permission_decorator('relatorioVenda')
 def listaVenda(request):
-    empreendimentos = Empreendimento.objects.filter(is_ativo=False).order_by('id')
+    empreendimentos = Empreendimento.objects.filter(is_ativo=True).order_by('id')
 
     vendas = RegisterVenda.objects.exclude(tipo_venda='RESERVADO').filter(
         Q(is_ativo=False) | Q(tipo_venda__in=['VENDIDO', 'CANCELADA'])
@@ -135,7 +148,10 @@ def listaVenda(request):
 
     # Filtrar por empreendimento
     if filtros['tipo_empreendimento']:
-        vendas = vendas.filter(lote__quadra__empr__id=filtros['tipo_empreendimento'])
+        vendas = vendas.filter(
+            lote__quadra__empr__id=filtros['tipo_empreendimento'],
+            lote__quadra__empr__is_ativo=True
+        )
 
     # Pesquisa textual
     if filtros['venda']:
@@ -335,8 +351,8 @@ def reserva_temporaria(request, lote_uuid):
 
 @has_permission_decorator('criarReservado')
 @transaction.atomic
-def criarReservado(request, id):
-    get_lote = get_object_or_404(Lote, id=id)
+def criarReservado(request, reserva_uuid):
+    get_lote = get_object_or_404(Lote, uuid=reserva_uuid)
     get_tempo = Empreendimento.objects.get(id=get_lote.quadra.empr_id)
     reserva_existente = RegisterVenda.objects.filter(lote=get_lote).first()
 
@@ -488,346 +504,3 @@ def deleteVenda(request, id):
     # return redirect('lista-venda')
 
 
-def proposta(request, venda_uuid):
-    venda = get_object_or_404(RegisterVenda, uuid=venda_uuid) #request.GET.get('venda_uuid'))
-    enderecoCliente = ClienteEndereco.objects.filter(id=venda.cliente.id)
-    contatoCliente = ClienteEndereco.objects.filter(id=venda.cliente.id)
-    conjuge = ClienteConjuge.objects.filter(id=venda.cliente.id)
-
-    # if not venda:
-    #   raise Http404("Venda não informada")
-
-    # try:
-    #    venda = RegisterVenda.objects.get(id=venda_id)
-    # except RegisterVenda.DoesNotExist:
-    #    raise Http404("Venda não encontrada")
-
-    locale.setlocale(locale.LC_TIME, 'pt_BR.UTF-8')
-
-    data_atual = timezone.now().date()
-
-    data_por_extenso = format_date(
-        data_atual,
-        format="d 'de' MMMM 'de' y",
-        locale='pt_BR'
-    )
-
-    get_tempo = venda.lote.quadra.empr
-
-    # ======================
-    # CÁLCULOS
-    # ======================
-    try:
-        area = float(venda.lote.area)
-        valor_metro = float(venda.lote.valor_metro_quadrado)
-        valor = area * valor_metro
-    except (TypeError, ValueError):
-        valor = 0
-
-    valor_total_formatado = f"R$ {valor:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
-
-    try:
-        total_parcelas = int(get_tempo.quantidade_parcela)
-    except (TypeError, ValueError, AttributeError):
-        total_parcelas = 0
-
-    try:
-        sinal = float(venda.valor_sinal)
-        valor_metro = float(venda.lote.valor_metro_quadrado)
-        valor_financiado = (area * valor_metro) - sinal
-    except (TypeError, ValueError):
-        valor_financiado = 0
-
-    valor_parcela = valor / total_parcelas if total_parcelas > 0 else 0
-    valor_parcela_formatado = f"R$ {valor_parcela:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
-    valor_sinal_formatado = f"R$ {float(venda.valor_sinal):,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
-    valor_financiado_formatado = f"R$ {valor_financiado:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
-
-    data_primeira_parcela = venda.dt_primeira_parcela
-
-    documento = get_object_or_404(
-        CadastroDocumento,
-        # tipo = 'venda'
-        id=1,  # 🔥 aqui está a mágica
-        ativo=True
-    )
-
-    template = Template(documento.texto)
-
-    html_final = template.render(Context({
-        'venda': venda,
-        'data_por_extenso': data_por_extenso,
-        'enderecoCliente': enderecoCliente,
-        'telefoneCliente': contatoCliente,
-        'valor_sinal_formatado': valor_sinal_formatado,
-        'valor_total_formatado': valor_total_formatado,
-        'valor_parcela_formatado': valor_parcela_formatado,
-        'valor_financiado_formatado': valor_financiado_formatado,
-        'data_primeira_parcela': data_primeira_parcela
-    }))
-
-    return HttpResponse(html_final)
-
-
-def proposta_pdf(request):
-    venda = get_object_or_404(RegisterVenda, id=request.GET.get('venda_id'))
-    enderecoCliente = ClienteEndereco.objects.filter(id=venda.cliente.id)
-    contatoCliente = ClienteEndereco.objects.filter(id=venda.cliente.id)
-    conjuge = ClienteConjuge.objects.filter(id=venda.cliente.id)
-
-    # if not venda:
-    #   raise Http404("Venda não informada")
-
-    # try:
-    #    venda = RegisterVenda.objects.get(id=venda_id)
-    # except RegisterVenda.DoesNotExist:
-    #    raise Http404("Venda não encontrada")
-
-    locale.setlocale(locale.LC_TIME, 'pt_BR.UTF-8')
-
-    data_atual = timezone.now().date()
-
-    data_por_extenso = format_date(
-        data_atual,
-        format="d 'de' MMMM 'de' y",
-        locale='pt_BR'
-    )
-
-    get_tempo = venda.lote.quadra.empr
-
-    # ======================
-    # CÁLCULOS
-    # ======================
-    try:
-        area = float(venda.lote.area)
-        valor_metro = float(venda.lote.valor_metro_quadrado)
-        valor = area * valor_metro
-    except (TypeError, ValueError):
-        valor = 0
-
-    valor_total_formatado = f"R$ {valor:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
-
-    try:
-        total_parcelas = int(get_tempo.quantidade_parcela)
-    except (TypeError, ValueError, AttributeError):
-        total_parcelas = 0
-
-    try:
-        sinal = float(venda.valor_sinal)
-        valor_metro = float(venda.lote.valor_metro_quadrado)
-        valor_financiado = (area * valor_metro) - sinal
-    except (TypeError, ValueError):
-        valor_financiado = 0
-
-    valor_parcela = valor / total_parcelas if total_parcelas > 0 else 0
-    valor_parcela_formatado = f"R$ {valor_parcela:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
-    valor_sinal_formatado = f"R$ {float(venda.valor_sinal):,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
-    valor_financiado_formatado = f"R$ {valor_financiado:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
-
-    data_primeira_parcela = venda.dt_primeira_parcela
-
-    documento = get_object_or_404(
-        CadastroDocumento,
-        # tipo = 'venda'
-        id=1,  # 🔥 aqui está a mágica
-        ativo=True
-    )
-
-    template = Template(documento.texto)
-
-    html_final = template.render(Context({
-        'venda': venda,
-        'nome_do_empreendimento': venda.lote.quadra.empr.nome,
-        'quadra': venda.lote.quadra.namequadra,
-        'lote': venda.lote.lote,
-        'area': venda.lote.area,
-        'cidade': venda.lote.quadra.empr.cidade,
-        'cliente_nome': venda.cliente.name,
-        'cliente_rg': venda.cliente.numero_rg,
-        'cliente_rg_emissor': venda.cliente.orgao_emissor_rg,
-        'cliente_cpf': venda.cliente.documento,
-        'cliente_email': venda.cliente.email,
-        'cliente_end': enderecoCliente,
-        'cliente_cep': enderecoCliente,
-        'cliente_telefone': contatoCliente,
-        'corretor_nome': venda.user.first_name,
-        'valor_sinal': venda.valor_sinal,
-        'valor_financiado': venda.valor_inicio_contrato,
-        'valor_venda': venda.valor_inicio_contrato,
-        'valor_parcela_formatado': valor_parcela_formatado,
-        'quantidade_parcelas': venda.quantidade_parcelas,
-        'data_primeira_parcela': data_primeira_parcela,
-        'data_por_extenso': data_por_extenso
-    }))
-
-    response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = 'inline; filename="documento.pdf"'
-
-    HTML(string=html_final).write_pdf(response)
-    return response
-
-
-"""def proposta(request):
-    venda_id = request.GET.get('venda_id')
-
-    if not venda_id:
-        raise Http404("Venda não informada")
-
-    try:
-        venda = RegisterVenda.objects.get(id=venda_id)
-    except RegisterVenda.DoesNotExist:
-        raise Http404("Venda não encontrada")
-
-    locale.setlocale(locale.LC_TIME, 'pt_BR.UTF-8')
-
-    enderecoCliente = ClienteEndereco.objects.filter(cliente=venda.cliente).first()
-    telefoneCliente = ClienteTelefone.objects.filter(cliente=venda.cliente).first()
-
-    data_atual = timezone.now().date()
-
-    data_por_extenso = format_date(
-        data_atual,
-        format="d 'de' MMMM 'de' y",
-        locale='pt_BR'
-    )
-
-    # 🔒 BUSCA ÚNICA + LOCK
-
-    get_tempo = venda.lote.quadra.empr
-
-    # ======================
-    # CÁLCULOS
-    # ======================
-    try:
-        area = float(venda.lote.area)
-        valor_metro = float(venda.lote.valor_metro_quadrado)
-        valor = area * valor_metro
-    except (TypeError, ValueError):
-        valor = 0
-
-    valor_total_formatado = f"R$ {valor:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
-
-    try:
-        total_parcelas = int(get_tempo.quantidade_parcela)
-    except (TypeError, ValueError, AttributeError):
-        total_parcelas = 0
-
-    try:
-        sinal = float(venda.valor_sinal)
-        valor_metro = float(venda.lote.valor_metro_quadrado)
-        valor_financiado = (area * valor_metro) - sinal
-    except (TypeError, ValueError):
-        valor_financiado = 0
-
-    valor_parcela = valor / total_parcelas if total_parcelas > 0 else 0
-    valor_parcela_formatado = f"R$ {valor_parcela:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
-    valor_sinal_formatado = f"R$ {float(venda.valor_sinal):,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
-    valor_financiado_formatado = f"R$ {valor_financiado:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
-
-    data_primeira_parcela = venda.dt_primeira_parcela
-
-    context = {
-        'venda': venda,
-        'data_por_extenso': data_por_extenso,
-        'enderecoCliente': enderecoCliente,
-        'telefoneCliente': telefoneCliente,
-        'valor_sinal_formatado': valor_sinal_formatado,
-        'valor_total_formatado': valor_total_formatado,
-        'valor_parcela_formatado': valor_parcela_formatado,
-        'valor_financiado_formatado': valor_financiado_formatado,
-        'data_primeira_parcela': data_primeira_parcela
-    }
-    return render(request, 'papeis/proposta.html', context)"""
-
-
-def preparar_contrato(request):
-    # 1. Buscar contrato ativo
-    contrato = get_object_or_404(Contrato, ativo=True)
-
-    # 2. Dados que virão do sistema (exemplo)
-    dados = {
-        'vendedor': 'Empresa XYZ LTDA',
-        'comprador': 'João da Silva',
-        'lote': '12',
-        'empreendimento': 'Residencial Sol Nascente',
-        'valor': '120.000,00',
-        'cidade': 'Fortaleza',
-        'data': now().strftime('%d/%m/%Y'),
-    }
-
-    # 3. Substituir variáveis do contrato
-    texto_processado = contrato.conteudo
-    for chave, valor in dados.items():
-        texto_processado = texto_processado.replace(
-            f'{{{{ {chave} }}}}', str(valor)
-        )
-
-    # 4. Retornar para teste (debug)
-    return HttpResponse(texto_processado)
-
-
-def visualizar_documento(request, venda_uuid):
-    venda = get_object_or_404(RegisterVenda, uuid=venda_uuid)
-    enderecoCliente = ClienteEndereco.objects.filter(id=venda.cliente.id)
-    contatoCliente = ClienteEndereco.objects.filter(id=venda.cliente.id)
-    conjuge = ClienteConjuge.objects.filter(id=venda.cliente.id)
-
-    documento = get_object_or_404(
-        CadastroDocumento,
-        #tipo = 'venda',
-        id=venda.lote.quadra.empr.contrato.id,  # 🔥 aqui está a mágica
-        ativo=True
-    )
-
-    template = Template(documento.texto)
-
-
-    html_final = template.render(Context({
-        'empreendimento': venda,
-        'comprador': venda.cliente,
-        'enderecoCliente': enderecoCliente,
-        'contatoCliente': contatoCliente,
-        'conjuge': conjuge,
-        # 'cpf': venda.cliente.cpf,
-        # 'lote': venda.lote.numero,
-        # 'quadra': venda.lote.quadra.nome,
-        # 'valor': venda.valor_total,
-        # 'data': venda.data_venda.strftime('%d/%m/%Y'),
-    }))
-
-    return HttpResponse(html_final)
-
-
-def documento_pdf(request):
-    venda = get_object_or_404(RegisterVenda, id=request.GET.get('venda_id'))
-    enderecoCliente = ClienteEndereco.objects.filter(id=venda.cliente.id)
-    contatoCliente = ClienteEndereco.objects.filter(id=venda.cliente.id)
-    conjuge = ClienteConjuge.objects.filter(id=venda.cliente.id)
-
-    documento = get_object_or_404(
-        CadastroDocumento,
-        # tipo = 'venda'
-        id=3,  # venda.lote.quadra.empr.contrato.id,  # 🔥 aqui está a mágica
-        ativo=True
-    )
-
-    template = Template(documento.texto)
-
-    html_final = template.render(Context({
-        'empreendimento': venda,
-        'comprador': venda.cliente,
-        'enderecoCliente': enderecoCliente,
-        'contatoCliente': contatoCliente,
-        'conjuge': conjuge,
-        # 'cpf': venda.cliente.cpf,
-        # 'lote': venda.lote.numero,
-        # 'quadra': venda.lote.quadra.nome,
-        # 'valor': venda.valor_total,
-        # 'data': venda.data_venda.strftime('%d/%m/%Y'),
-    }))
-
-    response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = 'inline; filename="documento.pdf"'
-
-    HTML(string=html_final).write_pdf(response)
-    return response

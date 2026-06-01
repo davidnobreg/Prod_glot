5d088bbe37c367691a1a3523c54e122b7c238911
David Nóbrega <davidsantos@ads.fiponline.edu.br>
2026-06-01 20:58:49 -0300

    fix: instalação do sentry

--- core/urls.py
+++ core/urls.py
@@ -1,23 +1,26 @@
 from django.contrib import admin
-from django.urls import path, include  # adicionar include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+
+from base.views import trigger_sentry_test
 
 urlpatterns = [
     path('', include('accounts.urls'), name='accounts'),
     path('admin/', admin.site.urls),
     path('clientes/', include('clientes.urls'), name='clientes'),
-
     path('dashboard/', include('dashboard.urls'), name='dashboard'),
     path('documentos/', include('documentos.urls'), name='documentos'),
     path('empreendimentos/', include('empreendimentos.urls'), name='empreendimentos'),
     # path('mensagem/', include('mensagem.urls'), name='message'),
     path('vendas/', include('vendas.urls'), name='vendas'),
+
+    # Remover após confirmar o Sentry
+    path('sentry-test/', trigger_sentry_test, name='entry_test'),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'base.views.not_found'
 handler403 = 'base.views.handler403'

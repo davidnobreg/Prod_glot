1991b8631788464395299daca61d633d501d642a
David Nóbrega <davidsantos@ads.fiponline.edu.br>
2026-06-01 19:09:42 -0300

    ci: docker compose

---
+++ docker/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+
+echo "Aplicando migrações..."
+python manage.py migrate --noinput
+
+echo "Coletando arquivos estáticos..."
+python manage.py collectstatic --noinput --ignore=admin
+
+exec "$@"
